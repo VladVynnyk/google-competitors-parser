df5781e403601d41464ae01a9749a0294ac051a5
Vladyslav Vynnyk <vladvynnyk1@gmail.com>
2024-04-02 10:46:37 +0300

    Improved parser: added handling more columns in excel

--- main.py
+++ main.py
@@ -6,39 +6,23 @@ from openpyxl import load_workbook
 from openpyxl import Workbook
 
 def is_similar(a, b):
-     return SequenceMatcher(None, a, b).ratio()
+    return SequenceMatcher(None, a, b).ratio()
 
 def parse_google_ads(url: str, name_of_product: str):
     response = requests.get(url)
-    # print(response)
  
     if response.status_code == 200:
-            # Отримуємо HTML-вміст сторінки
             html_content = response.text
  
-            # Створюємо об'єкт BeautifulSoup для парсингу
             soup = BeautifulSoup(html_content, 'html.parser')
  
-            # Знаходимо елементи, які містять рекламу
-            # ads = soup.find_all('div', class_='ads')
-
             ads_pannel = soup.find_all("div", id="bGmlqc")
-            # ads_name_headers = soup.html.find_all('span', class_='pymv4e', recursive=False)
             ads_name_headers = soup.html.find_all('h4', class_='fol5Z')
-            # ads_prices = soup.find_all('div', class_='qptdjc')
             ads_prices = soup.find_all('div', class_='pSNTSe')
             ads_items = soup.find_all("div", class_='pla-unit-container')
             ads_links = soup.html.find_all("a", class_="pla-unit-link")
             ads_images = soup.find_all("div", class_="Gor6zc")
             ads_sites = soup.find_all("div", class_="BZuDuc")
-
-            # print("Ads pannel: ", ads_pannel)
-            # print("Names: ", ads_name_headers)            
-            # print("Prices: ", ads_prices)      
-            # print("Items: ", ads_items)      
-            # print("Links: ", ads_links)
-            # print("Images", ads_images)
-            # print("Site: ", ads_sites)
 
             product_names = []
             product_prices = []
@@ -56,13 +40,9 @@ def parse_google_ads(url: str, name_of_product: str):
                 current_price = item.string
                 [new_price, another_value] = current_price.split(",")
                 numeric_part = ''.join(char for char in new_price if char.isdigit() or char == '.') 
-                # print(float(numeric_part))
-                # print(new_price)
-                # print("---------------------------------------")
                 product_prices.append(float(numeric_part))
 
             for item in ads_sites:
-                # print("ITEM: ", item.string)
                 product_sites.append(item.string)
             
 
@@ -77,7 +57,6 @@ def parse_google_ads(url: str, name_of_product: str):
                     product_info[name] = []
                 product_info[name].append([site, price])
 
-            # print(product_info)
 
             # STEPS: 
             # 1.Search for the cheapest products on the ads
@@ -85,12 +64,13 @@ def parse_google_ads(url: str, name_of_product: str):
             # 3.Search for the products on the iherb
             
             # 1.
-            workbook = load_workbook('D:\TEST OF MY JOB\EXCEL\Competitors parser\Каста ціни – копія.xlsx')
-            source_sheet = workbook.active
+            # Commented this section ---------------
+            # workbook = load_workbook('D:\TEST OF MY JOB\EXCEL\Competitors parser\Каста ціни – копія.xlsx')
+            # source_sheet = workbook.active
+            # --------------------------------------
 
 
-
-            smallest_values = heapq.nsmallest(2, product_prices)
+            smallest_values = heapq.nsmallest(3, product_prices)
             print("Smallest values: ", smallest_values)
 
             smallest_products = []
@@ -102,11 +82,16 @@ def parse_google_ads(url: str, name_of_product: str):
                         print("How similar: ", is_similar(name_of_product, item))
                         if is_similar(name_of_product, item) >= 0.3:
                             smallest_products.append(item)
-            
+
+            print("--------------------------------")
+            print("Cheapest products: ", smallest_products)
+            print("Length of cheapest products: ", len(smallest_values))
             result = []
+
             for product in smallest_products:
                 print("One of the cheapest products: ", product, product_info[product])
-                result.append(product_info[product])
+                # result.append(product_info[product])
+                result.append(product_info[product][0])
                 # return product_info[product]
             return result
 
@@ -116,7 +101,7 @@ def parse_google_ads(url: str, name_of_product: str):
 
 
 def main():
-    workbook = load_workbook('D:\TEST OF MY JOB\EXCEL\Competitors parser\Каста ціни – копія.xlsx')
+    workbook = load_workbook('./Testfile.xlsx')
     source_sheet = workbook.active
 
     products = []
@@ -143,13 +128,21 @@ def main():
 
             # Вставка значень info в клітинки J, K, L
             print("info:", info)
-            if info != None and len(info)>1:
-                source_sheet.cell(row=row_index, column=10).value = info[0][0][0]  # Значення info[0] у стовпець J
-                source_sheet.cell(row=row_index, column=11).value = info[0][0][1]  # Значення info[0] у стовпець J
-                if len(info[0]) == 3:
-                    source_sheet.cell(row=row_index, column=12).value = info[0][1][1]  # Значення info[1] у стовпець K
-                if len(info[0]) == 4:
-                    source_sheet.cell(row=row_index, column=13).value = info[0][2][1]  # Значення info[2] у стовпець L
+            if info != None and len(info)>=1:
+                source_sheet.cell(row=row_index, column=10).value = info[0][0]  # Значення info[0] у стовпець J
+                source_sheet.cell(row=row_index, column=11).value = info[0][1]  # Значення info[0] у стовпець J
+                print("SOURCE: ", info[0][0])
+                print("PRICE: ", info[0][1])
+                if len(info) == 2 or len(info) >= 2:
+                    source_sheet.cell(row=row_index, column=12).value = info[1][0]  # Значення info[1] у стовпець K
+                    source_sheet.cell(row=row_index, column=13).value = info[1][1]  # Значення info[1] у стовпець K
+                    print("SOURCE: ", info[1][0])
+                    print("PRICE: ", info[1][1])
+                if len(info) >= 3:
+                    source_sheet.cell(row=row_index, column=14).value = info[2][0]  # Значення info[2] у стовпець L
+                    source_sheet.cell(row=row_index, column=15).value = info[2][1]  # Значення info[2] у стовпець L
+                    print("SOURCE: ", info[2][0])
+                    print("PRICE: ", info[2][1])
                 row_index += 1
             else:
                 row_index += 1
@@ -157,7 +150,7 @@ def main():
 
             # row_index += 1  # Збільшення індексу рядка для наступної вставки
 
-    workbook.save('D:\TEST OF MY JOB\EXCEL\Competitors parser\Каста ціни – копія.xlsx')
+    workbook.save('./Testfile.xlsx')
 
 
 main()

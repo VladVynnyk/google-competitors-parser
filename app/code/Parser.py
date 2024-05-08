9f97d940710b389490abd9c5e94f44e0fcf2cc02
Vladyslav Vynnyk <vladvynnyk1@gmail.com>
2024-05-08 12:10:18 +0300

    Improved integration with parser and ai. Improved writing to excel file.

--- app/code/Parser.py
+++ app/code/Parser.py
@@ -1,3 +1,4 @@
+import ast
 import requests
 from openai import OpenAI
 from bs4 import BeautifulSoup
@@ -6,11 +7,12 @@ from utils import get_random_user_agent, format_price, create_messages_for_ai, i
 
 
 class Parser:
-    def __init__():
+    def __init__(self):
         pass
 
     # This method returns ads with lowest price
-    def parse_google_ads(url: str, name_of_product: str, client_of_ai):
+    def parse_google_ads(self, url: str, name_of_product: str, client_of_ai):
+        # {product1: {source: Bigl.ua, price:1000, link:https://}, product2: {source: Bigl.ua, price:1000, link:https://}}
         user_agent = get_random_user_agent()
 
         print("user-agent: ", user_agent)
@@ -121,3 +123,102 @@ class Parser:
                     print("One of the cheapest products: ", product, product_info[product])
                     result.append(product_info[product][0])
                 return result
+        
+
+
+    # This method returns ads with lowest price
+    def parse_google_ads(self, url: str, name_of_product: str, client_of_ai):
+        user_agent = get_random_user_agent()
+
+        print("user-agent: ", user_agent)
+
+        headers = {'User-agent': user_agent}
+
+
+        response = requests.get(url, headers=headers)
+
+        # with open("demofile2.txt", "w", encoding="utf-8") as f:
+        #     f.write(response.text)
+    
+        if response.status_code == 200:
+                html_content = response.text
+    
+                soup = BeautifulSoup(html_content, 'html.parser')
+
+                ads_pannel = soup.find_all("div", id="top-pla-group-inner")
+                ads_name_headers = soup.html.find_all('a', class_='pla-unit-title-link')
+                ads_prices = soup.find_all('div', class_='T4OwTb')
+                ads_items = soup.find_all("div", class_='pla-unit-container')
+                ads_links = soup.find_all("a", class_="pla-unit-img-container-link")
+                ads_images = soup.find_all("div", class_="Gor6zc")
+                ads_sites = soup.find_all("div", class_="LbUacb")
+
+                product_names = []
+                product_prices = []
+                product_sites = []
+                product_links = []
+
+                for a_tag in ads_name_headers:
+                    current_name = a_tag.find("span").text
+                    print("CURRENT NAME: ", current_name)
+                    product_names.append(current_name)
+
+
+                for link in ads_links:
+                    print("EXTRACTED PRODUCT LINK: ", extract_product_link(link['href']))
+                    print("SINGLE LINK: ", link['href'])
+                    current_link = link['href']
+                    product_links.append(current_link)
+
+
+                for item in ads_prices:
+                    current_price = item.find('span').text
+                    formatted_price = format_price(current_price)
+                    print("Current price: ", formatted_price)
+                    [new_price, another_value] = current_price.split(",")
+                    numeric_part = ''.join(char for char in new_price if char.isdigit() or char == '.') 
+                    product_prices.append(float(numeric_part))
+
+                for item in ads_sites:
+                    product_sites.append(item.string)
+                    
+                product_info = {}    
+                    
+                for name, price, site, link in zip(product_names, product_prices, product_sites, product_links):
+                    if name not in product_info:
+                        product_info[name] = {}
+                    product_info[name] = {"site": site, "price": price, "link": link}
+
+                print("INFORMATION: ", product_info)
+                print("Len: ", len(product_info))
+
+                # Here we should understand, which products are relevant
+                messages = create_messages_for_ai(name_of_product, product_names)
+                print("MESSAGES: ", messages)
+                if len(messages) > 0: 
+                    completion = client_of_ai.chat.completions.create(
+                        model="gpt-3.5-turbo-0125",
+                        messages=messages
+                    )            
+                
+                relevant_products = completion.choices[0].message.content
+                # relevant_products = list(relevant_products)
+                relevant_products = ast.literal_eval(relevant_products)
+                print("Relevant products: ", relevant_products)
+
+                result = {}
+
+                for product in relevant_products:
+                    if product_info.get(product) is not None:
+                        print("Key existing: ", product)
+                        result[product] = product_info.get(product)
+                    else:
+                        print("Key is not existing: ", product)
+                        continue
+
+                print("Len: ", len(result))
+                
+                sorted_data = dict(sorted(result.items(), key=lambda x: x[1]['price']))
+                # {product1: {source: Bigl.ua, price:1000, link:https://}, product2: {source: Bigl.ua, price:1000, link:https://}}
+                
+                return sorted_data
--- app/code/main_new_version.py
+++ app/code/main_new_version.py
@@ -17,13 +17,14 @@ from utils import get_random_user_agent, format_price, create_messages_for_ai, i
 
 
 config = dotenv_values(".env")
-CHATGPT_KEY = config["CHATGPT_KEY"]
+print("config " , config)
+CHATGPT_API_KEY = config["CHATGPT_API_KEY"]
  
 def main():
     parser = Parser()
-    client = OpenAI(api_key=CHATGPT_KEY)
+    client = OpenAI(api_key=CHATGPT_API_KEY)
 
-    workbook = load_workbook('app/data/Testfile.xlsx')
+    workbook = load_workbook('./Testfile.xlsx')
     source_sheet = workbook.active
 
     max_row = source_sheet.max_row
@@ -40,46 +41,29 @@ def main():
             print(edited_product_name)
             print(url)
             info = parser.parse_google_ads(url, cell.value, client_of_ai=client)
-            # {product1: [source, price, link], product2: [source, price, link]}
+            # {product1: {source: Bigl.ua, price:1000, link:https://}, product2: {source: Bigl.ua, price:1000, link:https://}}
 
             print("info:", info)
-            time.sleep(10)
-
-            # Вставка значень info в клітинки J, K, L
-            if info != None and len(info)>=1:
-                source_sheet.cell(row=row_index, column=10).value = info[0][2]  # Значення info[0] у стовпець J
-                source_sheet.cell(row=row_index, column=9).value = info[0][1]  # Значення info[0] у стовпець J
-                print("SOURCE: ", info[0][0])
-                print("PRICE: ", info[0][1])
-                print("LINK: ", info[0][2])
-                if len(info) == 2 or len(info) >= 2:
-                    source_sheet.cell(row=row_index, column=13).value = info[1][2]  # Значення info[1] у стовпець K
-                    source_sheet.cell(row=row_index, column=12).value = info[1][1]  # Значення info[1] у стовпець K
-                    print("SOURCE: ", info[1][0])
-                    print("PRICE: ", info[1][1])
-                    print("LINK: ", info[1][2])
-                if len(info) == 3:
-                    source_sheet.cell(row=row_index, column=16).value = info[2][2]  # Значення info[2] у стовпець L
-                    source_sheet.cell(row=row_index, column=15).value = info[2][1]  # Значення info[2] у стовпець L
-                    print("SOURCE: ", info[2][0])
-                    print("PRICE: ", info[2][1])
-                    print("LINK: ", info[2][2])
-                if len(info) == 4:
-                    source_sheet.cell(row=row_index, column=19).value = info[3][2]  # Значення info[2] у стовпець L
-                    source_sheet.cell(row=row_index, column=18).value = info[3][1]  # Значення info[2] у стовпець L
-                    print("SOURCE: ", info[2][0])
-                    print("PRICE: ", info[2][1])
-                    print("LINK: ", info[2][2])
-                if len(info) == 5:
-                    source_sheet.cell(row=row_index, column=22).value = info[4][2]  # Значення info[2] у стовпець L
-                    source_sheet.cell(row=row_index, column=21).value = info[4][1]  # Значення info[2] у стовпець L
-                    print("SOURCE: ", info[2][0])
-                    print("PRICE: ", info[2][1])
-                    print("LINK: ", info[2][2])
+            print("info:", len(info))
+            # time.sleep(10)
+            
+            if info is not None and len(info) >= 1:
+                column_index = 10  # Starting column index
+                for key, value in info.items():
+                    source_sheet.cell(row=row_index, column=column_index).value = value['link']
+                    source_sheet.cell(row=row_index, column=column_index + 1).value = value['price']
+                    print("PRICE:", value['price'])
+                    print("LINK:", value['link'])
+                    column_index += 2  # Move to the next pair of columns
                 row_index += 1
+
+                workbook.save('./Оборотні_профільних_ціни_конкурентів_для_HF.xlsx')
+                time.sleep(10)
             else:
                 row_index += 1
                 continue
+
+
 
 
     # workbook.save('./Testfile.xlsx')
